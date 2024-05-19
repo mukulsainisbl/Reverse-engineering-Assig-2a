49b16c8e8d407f062ce7a25a6e0e211cc3d98a12
Mukul Saini <138042294+mukulsainisbl@users.noreply.github.com>
2024-05-19 14:32:57 +0530

    Axious get

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import "../App.css"
+
+function Post({ title, body }) {
+  return (
+    <div id="post-box">
+      <p>Title : {title}</p>
+      <p>Body : {body}</p>
+    </div>
+  );
+}
+
+export default Post;
